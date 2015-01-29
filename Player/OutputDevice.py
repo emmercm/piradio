f822a6426d594e2753245b282c86c0d25eb1630a
Christian Emmer <emmercm@gmail.com>
2015-01-28 22:46:43 -0500

    (Non-Functional) Initial class structure, sample free-use audio file, attempt at using VLC through Python (may need to switch to a pure Python library).

---
+++ Player/OutputDevice.py
@@ -0,0 +1,24 @@
+import abc
+
+class OutputDevice(object):
+	__metaclass__ = abc.ABCMeta
+	
+	# @abc.abstractmethod
+	# def Open(self, filename):
+		# return
+	
+	# @abc.abstractmethod
+	# def Play(self):
+		# return
+	# @abc.abstractmethod
+	# def Pause(self):
+		# return
+	# @abc.abstractmethod
+	# def Stop(self):
+		# return
+	# @abc.abstractmethod
+	# def Prev(self):
+		# return
+	# @abc.abstractmethod
+	# def Next(self):
+		# return

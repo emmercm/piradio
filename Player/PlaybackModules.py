41d290d192bd489744a76f7397ba5c7a67ab97bd
Christian Emmer <emmercm@gmail.com>
2015-01-30 00:29:08 -0500

    Changed VLC to use playlists instead of individual media.

--- Player/PlaybackModules.py
+++ Player/PlaybackModules.py
@@ -6,7 +6,7 @@ class PlaybackModule(object):
 	__metaclass__ = abc.ABCMeta
 	
 	@abc.abstractmethod
-	def Open(self, filename):
+	def Add(self, filename):
 		return
 	
 	@abc.abstractmethod
@@ -18,12 +18,12 @@ class PlaybackModule(object):
 	@abc.abstractmethod
 	def Stop(self):
 		return
-	# @abc.abstractmethod
-	# def Prev(self):
-		# return
-	# @abc.abstractmethod
-	# def Next(self):
-		# return
+	@abc.abstractmethod
+	def Prev(self):
+		return
+	@abc.abstractmethod
+	def Next(self):
+		return
 		
 	# @abc.abstractmethod
 	# def SetVolume(self, vol):
@@ -35,11 +35,13 @@ class VLCPlayback(PlaybackModule):
 	def __init__(self, *args):
 		# CME NOTE: After a clean install of pulseaudio on raspbian VLC stops working. Force ALSA to potentially fix.
 		self.vlc_instance = vlc.Instance('--aout alsa')
-		self.vlc_player = self.vlc_instance.media_player_new()
+		self.vlc_playlist = self.vlc_instance.media_list_new()
+		self.vlc_player = self.vlc_instance.media_list_player_new()
+		self.vlc_player.set_media_list(self.vlc_playlist)
 		
-	def Open(self, filename):
+	def Add(self, filename):
 		media = self.vlc_instance.media_new(filename)
-		self.vlc_player.set_media(media)
+		self.vlc_playlist.add_media(media)
 		
 	def Play(self):
 		self.vlc_player.play()
@@ -47,3 +49,9 @@ class VLCPlayback(PlaybackModule):
 		self.vlc_player.pause()
 	def Stop(self):
 		self.vlc_player.stop()
+	def Prev(self):
+		if self.vlc_player.previous() == -1:
+			self.Stop()
+	def Next(self):
+		if self.vlc_player.next() == -1:
+			self.Stop()
--- piradio.py
+++ piradio.py
@@ -5,7 +5,10 @@ import time
 import Player
 
 playback_module = Player.PlaybackModules.VLCPlayback()
-playback_module.Open("Peppy--The-Firing-Squad_YMXB-160.mp3")
+playback_module.Add("Peppy--The-Firing-Squad_YMXB-160.mp3")
+playback_module.Add("Peppy--The-Firing-Squad_YMXB-160.mp3")
 playback_module.Play()
-time.sleep(10)
+time.sleep(5)
+playback_module.Next()
+time.sleep(5)
 playback_module.Stop()

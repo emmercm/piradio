139196f6b92549483684c9f553a2e787b1aa490a
Christian Emmer <emmercm@gmail.com>
2015-02-11 21:35:15 -0500

    Setup: Changed a lot of sys.stdout.write() to plain prints, added pip to pip package list (to keep it updated).

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def which(bin):
 
 
 if os.geteuid() != 0:
-	sys.stdout.write("PiRadio setup requires root priveleges.\n")
+	print "PiRadio setup requires root priveleges."
 	sys.exit(1)
 	
 	
@@ -24,30 +24,25 @@ modules = subprocess.check_output("cat /proc/modules", shell=True)
 if not "i2c_dev" in modules:
 	with open('/etc/modules', 'r+') as f:
 		if not "i2c-dev" in f.read():
-			sys.stdout.write("I2C module is not loaded and is not in /etc/modules, adding...\n")
-			sys.stdout.flush()
+			print "I2C module is not loaded and is not in /etc/modules, adding..."
 			f.write("i2c-dev\n")
-			sys.stdout.write("Reboot recommended.\n")
-			sys.stdout.flush()
+			print "Reboot recommended."
 	
 	
 # Import/install+import APT
 try:
 	import apt
 except ImportError:
-	sys.stdout.write("Installing python-apt...\n")
-	sys.stdout.flush()
+	print "Installing python-apt..."
 	os.system("sudo apt-get install python-apt")
 	try:
 		import apt
 	except ImportError:
-		sys.stdout.write("python-apt install fail\n")
-		sys.stdout.flush()
+		print "python-apt install fail"
 		sys.exit(1)
 	else:
-		sys.stdout.write("python-apt install success\n")
-	sys.stdout.write("\n")
-	sys.stdout.flush()
+		print "python-apt install success"
+	print ""
 
 # Prep APT cache for usage
 sys.stdout.write("Getting APT cache... ")
@@ -62,61 +57,53 @@ apt_commit = False
 for package_name in ["python-dev","python-smbus","vlc"]:
 	apt_package = apt_cache[package_name]
 	if not apt_package.is_installed:
-		sys.stdout.write("Marking "+package_name+" for install\n")
-		sys.stdout.flush()
+		print "Marking "+package_name+" for install"
 		apt_package.mark_install()
 		apt_commit = True
 if apt_commit:
 	try:
 		apt_cache.commit(install_progress=None)
 	except Exception, arg:
-		sys.stdout.write("APT install fail\n")
+		print "APT install fail"
 	else:
-		sys.stdout.write("APT install success\n")
-	sys.stdout.write("\n")
-	sys.stdout.flush()
+		print "APT install success"
+	print ""
 	
 	
 # Install pip if missing
 if which("pip") == False:
-	sys.stdout.write("Downloading pip installer... ")
-	sys.stdout.flush()
+	print "Downloading pip installer..."
 	if os.path.isfile("get-pip.py"):
 		os.remove("get-pip.py")
 	urllib.urlretrieve("https://bootstrap.pypa.io/get-pip.py", "get-pip.py")
 	if not os.path.isfile("get-pip.py"):
-		sys.stdout.write("fail\n")
+		print "Download fail"
 	else:
-		sys.stdout.write("success\n")
-		sys.stdout.write("Installing pip...\n")
-		sys.stdout.flush()
+		print "Download success"
+		print "Installing pip..."
 		os.system("sudo python get-pip.py")
 		if os.path.isfile("get-pip.py"):
 			os.remove("get-pip.py")
 		if which("pip") == False:
-			sys.stdout.write("pip install fail\n")
+			print "pip install fail"
 		else:
-			sys.stdout.write("pip install success\n")
-	sys.stdout.write("\n")
-	sys.stdout.flush()
+			print "pip install success"
+	print ""
 	
 	
 # Installing missing pip packages
 if which("pip") == True:
 	pip_list = subprocess.check_output("pip list", shell=True)
-	for package_name in ["cherrypy","formencode","genshi","dot3k"]:
+	for package_name in ["pip","cherrypy","formencode","genshi","dot3k"]:
 		found = False
 		for line in pip_list.splitlines():
 			if line.split(" ")[0].lower() == package_name.lower():
 				found = True
 				break
 		if found == False:
-			sys.stdout.write("Installing "+package_name+"...\n")
-			sys.stdout.flush()
+			print "Installing "+package_name+"..."
 			os.system("sudo pip install "+package_name)
 		else:
-			sys.stdout.write("Upgrading "+package_name+"...\n")
-			sys.stdout.flush()
+			print "Upgrading "+package_name+"..."
 			os.system("sudo pip install --upgrade "+package_name)
-	sys.stdout.write("\n")
-	sys.stdout.flush()
+	print ""

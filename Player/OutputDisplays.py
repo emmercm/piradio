dafc376e6681471b11296b7887354cf7060d7f8c
Christian Emmer <emmercm@gmail.com>
2015-04-20 21:46:57 -0400

    Player: More complete OutputDisplays comments.

--- Player/OutputDisplays.py
+++ Player/OutputDisplays.py
@@ -19,6 +19,7 @@ class Menu(object):
 		self._line = 0
 		self._lines = lines
 		
+	# Getter/setter for self._line, clamp the set value
 	def line_get(self):
 		return self._line
 	def line_set(self, value):
@@ -29,15 +30,18 @@ class Menu(object):
 		self._line = value
 	line = property(line_get, line_set)
 	
+	# Return a list of menu item text
 	def MenuKeys(self):
 		items = copy.copy(self._menu)
 		for idx, item in enumerate(items):
 			items[idx] = item[0]
 		return items
 		
+	# Return the active menu item
 	def GetItem(self):
 		return self._menu[self.line]
 		
+	# Calculate the menu items to show on the LCD
 	def CalcLineStartEnd(self):
 		# Calculate line_start
 		line_start = self.line - int(math.floor(self._lines/2))
@@ -49,10 +53,12 @@ class Menu(object):
 			line_start = line_end - self._lines
 			if line_start < 0: line_start = 0
 		return line_start, line_end
+	# Calculate the active LCD line
 	def CalcLineCurr(self):
 		line_start, line_end = self.CalcLineStartEnd()
 		return self.line - line_start
 		
+	# Return text to printed to the LCD
 	def GetLines(self, offset=0):
 		line_start, line_end = self.CalcLineStartEnd()
 		items = self.MenuKeys()
@@ -84,17 +90,20 @@ class OutputDisplay(object):
 		self.last_event = time.time()
 		
 		
+	# Create a new menu, append it to the menu list, print it
 	def MenuOpen(self, menu, start=0):
 		menu_obj = Menu(menu, self.display_height)
 		menu_obj.line = start
 		self.menus.append(menu_obj)
 		self.MenuPrint()
 		
+	# Return the current menu
 	def MenuCurr(self):
 		if len(self.menus) == 0:
 			return None
 		return self.menus[len(self.menus)-1]
 		
+	# Print the current menu to LCD, or optionally only one line
 	def MenuPrint(self, line_num=None):
 		while self.printing: # allow only one MenuPrint() at once
 			time.sleep(0.05)
@@ -111,6 +120,7 @@ class OutputDisplay(object):
 					self.PrintLine(idx, line)
 		self.printing = False
 		
+	# The 'up' button has been pressed, if a menu is open change the active item and refresh
 	def HandleUp(self):
 		self.events['ButtonUp'] = True
 		if self.MenuCurr().Paused == False:
@@ -119,6 +129,7 @@ class OutputDisplay(object):
 			self.display_offset_timer = time.time()
 			self.MenuPrint()
 		self.last_event = time.time()
+	# The 'down' button has been pressed, if a menu is open change the active item and refresh
 	def HandleDown(self):
 		self.events['ButtonDown'] = True
 		if self.MenuCurr().Paused == False:
@@ -128,16 +139,20 @@ class OutputDisplay(object):
 			self.MenuPrint()
 		self.last_event = time.time()
 		
+	# The 'right' button has been pressed, raise the ButtonForward event
 	def HandleForward(self):
 		self.events['ButtonForward'] = True
 		self.last_event = time.time()
+	# The 'left' button has been pressed, raise the ButtonBack event
 	def HandleBack(self):
 		self.events['ButtonBack'] = True
 		self.last_event = time.time()
+	# The 'center' button has been pressed, raise the ButtonSelect event
 	def HandleSelect(self):
 		self.events['ButtonSelect'] = True
 		self.last_event = time.time()
 		
+	# Open a menu, display it, and handle calling selected items
 	def DisplayMenu(self, menu, start=0):
 		self.display_offset = 0 # needs to happen before MenuOpen()
 		self.display_offset_timer = time.time()
@@ -154,7 +169,7 @@ class OutputDisplay(object):
 				if item == None:
 					pass
 				elif type(item) is dict:
-					# CME TODO: Nested menus
+					# TODO: Nested menus
 					pass
 				else:
 					self.MenuCurr().Paused = True
@@ -169,13 +184,15 @@ class OutputDisplay(object):
 						self.display_offset_timer = time.time() # reset active item offset timer
 						self.MenuPrint() # redraw just in case
 					self.MenuCurr().Paused = False
-				
+					
+			# Close the menu, pop the menu, (and cause the while loop to break)
 			if 'ButtonBack' in self.events:
 				self.events.pop('ButtonBack',None)
 				if menu_depth > 1:
 					self.menus.pop()
 					self.MenuPrint()
 					
+			# If the active menu item is too long scroll its text
 			if len(self.MenuCurr().GetItem()[0]) > self.display_width - 2:
 				if (self.display_offset_timer + 1.25) <= time.time():
 					self.display_offset += 1
@@ -184,6 +201,7 @@ class OutputDisplay(object):
 					self.MenuPrint(self.MenuCurr().CalcLineCurr())
 					self.display_offset_timer = time.time() - 1
 					
+			# After 5s of inactivity display the active track
 			if (self.last_event + 5) <= time.time() and __builtin__.PlaybackModule != None and __builtin__.PlaybackModule.IsLoaded():
 				self.DisplayTrack()
 				self.MenuPrint()
@@ -194,26 +212,30 @@ class OutputDisplay(object):
 		return 1
 		
 		
+	# Display information about the active track
 	def DisplayTrack(self):
 		self.Clear()
 		self.events = {}
-		self.MenuCurr().Paused = True
+		self.MenuCurr().Paused = True # make sure menu isn't handling input
 		
 		status_curr = None
 		status_offset = 0
 		status_offset_timer = time.time()
 		
 		while True:
+			# The 'center' button was pressed, toggle playback
 			if 'ButtonSelect' in self.events:
 				self.events.pop('ButtonSelect',None)
 				__builtin__.PlaybackModule.Toggle()
+			# The 'up' button was pressed, skip to previous track
 			if 'ButtonUp' in self.events:
 				self.events.pop('ButtonUp',None)
 				__builtin__.PlaybackModule.Prev()
+			# The 'down' button was pressed, skip to next track
 			if 'ButtonDown' in self.events:
 				self.events.pop('ButtonDown',None)
 				__builtin__.PlaybackModule.Next()
-				
+			# The 'left' button was pressed, return back to the active menu
 			if 'ButtonBack' in self.events:
 				self.events.pop('ButtonBack',None)
 				break
@@ -238,6 +260,7 @@ class OutputDisplay(object):
 						else:
 							self.PrintLine(1, status_new['title'])
 							
+					# Print playback status, playback elapsed/length time
 					out_time = '>' if __builtin__.PlaybackModule.IsPlaying() else '#'
 					out_time += ' ' + status_new['elapsed_display']
 					if status_new['length'] > 0:
@@ -246,9 +269,9 @@ class OutputDisplay(object):
 				
 				status_curr = status_new
 				
+			# If the displayed track is too long scroll its text
 			if (status_offset_timer + 1.25) <= time.time():
 				status_offset += 1
-				
 				if self.display_height == 3:
 					if status_offset > len(status_curr['artist']) and status_offset > len(status_curr['title']): # offset greater than artist and title
 						status_offset = 0
@@ -256,7 +279,6 @@ class OutputDisplay(object):
 						self.PrintLine(0, (status_curr['artist'])[status_offset:])
 					if len(status_curr['title']) > self.display_width: # print offset title line
 						self.PrintLine(1, (status_curr['title'])[status_offset:])
-						
 				status_offset_timer = time.time() - 1
 				
 			time.sleep(0.05)
@@ -266,10 +288,12 @@ class OutputDisplay(object):
 		return 0
 		
 		
+	# Clear the LCD display completely
 	@abc.abstractmethod
 	def Clear(self):
 		pass
 		
+	# Print a single line to LCD
 	@abc.abstractmethod
 	def PrintLine(self, line, str):
 		pass
@@ -301,8 +325,6 @@ class DisplayOTron3k(OutputDisplay):
 		super(DisplayOTron3k, self).__init__(*args)
 		self.display_height = 3
 		self.display_width = 16
-		
-		dot3k.backlight.rgb(150,150,150)
 		
 		# Handle up button
 		@dot3k.joystick.on(dot3k.joystick.UP)
@@ -342,9 +364,11 @@ class DisplayOTron3k(OutputDisplay):
 		jukebox = Jukebox()
 		jukebox.start()
 		
+	# Clear the LCD display completely
 	def Clear(self):
 		dot3k.lcd.clear()
 		
+	# Print a single line to LCD
 	def PrintLine(self, line, text):
 		dot3k.lcd.set_cursor_position(0, line)
 		dot3k.lcd.write(text[:self.display_width].ljust(self.display_width))
